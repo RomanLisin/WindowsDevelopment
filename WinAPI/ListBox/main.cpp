14db50fcfb11f4fbde7afd907e4331e4c6bbed0c
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-11-10 15:10:05 +1000

    ListBox done AddDelete add

---
+++ WinAPI/ListBox/main.cpp
@@ -0,0 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS  // добавляем при ошибке С4996
+#include<Windows.h>
+#include<cstdio>  // чтобы работал sprintf(...)
+#include<cstring>
+#include"resource.h"
+
+CONST CHAR* init_values[] = { "Привет", "This", "is", "my", "first", "Combo", "Box" };
+
+BOOL CALLBACK DlgProc(HWND nwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK InputDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+	return 0;
+
+}
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);    // функция GetDlgItem() возвращает HWND указанного дочерего окна
+		for (int i = 0; i < sizeof(init_values) / sizeof(init_values[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)init_values[i]);
+		}
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			CHAR sz_message[SIZE]{};
+			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			// Сообщение LB_GETCURSEL (Current Selection) возвращает номер выбранного пункта ListBox
+			SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			sprintf(sz_message, "Вы выбрали пункт N%i со значением \"%s\".", i, sz_buffer);
+			MessageBox(hwnd, sz_message, "Selected value", MB_OK | MB_ICONINFORMATION);
+			break;
+		}
+		case IDC_BUTTON1:
+		{
+			// CHAR sz_newItem[256] = "";
+				HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+			if (DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hwnd, InputDlgProc, (LPARAM)hListBox)==IDOK) //sz_newItem) == IDOK)
+			{
+
+				//if (sz_newItem[0] != "\0")
+			/*	if (strlen(sz_newItem)>0)
+				{
+					SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_newItem);
+				}*/
+			}
+				break;
+		}
+		case IDC_BUTTON2: 
+		{
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			if (i != LB_ERR)
+			{
+				SendMessage(hListBox, LB_DELETESTRING, i, 0);
+			}
+			else
+			{
+				MessageBox(hwnd, "Выберите элемент для удаления", "Ошибка", MB_OK | MB_ICONERROR);
+			}
+			break;
+		}
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
+BOOL CALLBACK InputDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hListBox; 
+	CHAR buffer[256] = "";
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		hListBox = (HWND)lParam;
+		return TRUE;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			// Считываем введенный текст и сохраняем в lParam для передачи в основное окно
+			//CHAR* buffer = (CHAR*)lParam;
+			GetDlgItemText(hwnd, IDC_EDIT1, buffer, 256);
+			if (strlen(buffer) > 0)
+			{
+				SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)buffer);
+			}
+			EndDialog(hwnd, IDOK);
+			return TRUE;
+		}
+		break;
+		case IDCANCEL:
+			EndDialog(hwnd, IDCANCEL);
+			break;
+		}
+		break;
+	}
+	return FALSE;
+}
--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<Windows.h>
 #include "resource.h"
 
+
+CONST CHAR* init_value = "Text here";
 //#define MESSAGE_BOX
 BOOL CALLBACK DlgProc(HWND  hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 // первый параметр дескриптор экземпляра приложения, второй не использ., третий предоставляет доступ к переменным cmd, режим отображения окна , иногда указывается в качестве второго аргумента функции ShowWindow(hwnd, iShow)
@@ -26,14 +28,30 @@ BOOL CALLBACK DlgProc(HWND  hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 	{
+		HWND hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		SetWindowText(hEdit1, init_value); // первоначальный текст
 	}
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
 		case IDOK: MessageBox(hwnd, "Была нажата кнока OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDC_BUTTON1:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			// получаем текст из первого окна редактирования
+			HWND hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+			GetWindowText(hEdit1, sz_buffer, SIZE);
+
+			// устанавливаем текст во второе окно редактирования
+			HWND hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+			SetWindowText(hEdit2, sz_buffer);
+			break;
+		}
+
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 		break;
