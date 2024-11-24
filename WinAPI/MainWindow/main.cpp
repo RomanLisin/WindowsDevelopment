08bc56aee6f42009a5131df92308eac1d712010e
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-11-24 21:26:23 +1000

    HomeWork DONE

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,5 +1,8 @@
 //MainWindow
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
+#include"resource.h"
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Main Window";
 
@@ -17,9 +20,13 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0; // cb .... - CountBytes;
 
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	//wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);//(NULL, IDI_APPLICATION);
+	//wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hIcon = (HICON)LoadImage(NULL, "ICO\\icon1.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wClass.hIconSm = (HICON)LoadImage(NULL, "ICO\\palm16x16.ico",IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+
+	wClass.hCursor = (HCURSOR)LoadImage(hInstance, 
+		"ANI\\3work.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
 	wClass.hInstance = hInstance;
@@ -33,14 +40,20 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		return 0;
 	}
 	 //2)Создание окна
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+	INT window_width = screen_width * 3 / 4;
+	INT window_height = screen_height * 0.75;
+	INT window_start_x = screen_width / 8;
+	INT window_start_y = screen_height / 8;
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,    // Ex-Style
 		g_sz_WINDOW_CLASS, // Class name
 		g_sz_WINDOW_CLASS, // Window title
 		WS_OVERLAPPEDWINDOW, // Window stile. такой стиль всегда задаётся для главного окна
-		CW_USEDEFAULT, CW_USEDEFAULT,   // Position - положение окна при запуске
-		CW_USEDEFAULT, CW_USEDEFAULT,   // Size - размер создаваемого окна
+		window_start_x, window_start_y,   // Position - положение окна при запуске
+		window_width, window_height, //   CW_USEDEFAULT, CW_USEDEFAULT,   // Size - размер создаваемого окна
 		NULL,  // Родительского окна нет
 		NULL,  //  hMenu  - для главного окна это ResorceID главного меню, для дочернего окна (элемента какого-то окна) 
 		//  это ResourceID соответствующего элемента. По этому ResourceID нужный элемент всегда можно получить
@@ -73,6 +86,31 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
+		break;
+	case WM_MOVE:
+
+		break;
+	case WM_SIZE:
+	{
+		RECT window_rect;
+		GetWindowRect(hwnd, &window_rect);
+		INT window_width = window_rect.right - window_rect.left;
+		INT window_height = window_rect.bottom - window_rect.top;
+		
+
+
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+		sprintf
+		(
+			sz_title, "%s. Position:%ix%i; Size: %ix%i", 
+			g_sz_WINDOW_CLASS, 
+			window_rect.left, window_rect.top,
+			window_width, window_height
+		);
+		//SetWindowText(hwnd,sz_title);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}
 		break;
 	case WM_COMMAND:
 		break;
