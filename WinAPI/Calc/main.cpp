8cc03ffc70faf034c72051c02b4f40723687d224
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-12-23 00:04:28 +1000

    LoadFontsFromDLl function DONE

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -5,6 +5,9 @@
 #include<float.h>
 #include<cstdio>
 #include"Colors.h"
+//#include"Skin.h"
+#include"Fonts.h"
+
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc_VPD_311";
 
 CONST CHAR* g_OPERATION[] = { "+" ,"-", "*","/" };
@@ -14,6 +17,8 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 VOID SetSkin(HWND hwnd, CONST CHAR skin[]);
 VOID SetSkinFromDLL(HWND hwnd, CONST CHAR skin[]);
+VOID LoadFontFromDLL(HMODULE hFontModule, INT resourceID);
+VOID LoadFontFromDLL(HMODULE hFontsModule);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -74,7 +79,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	
 	static INT index = 0;
 	static HMODULE hFontsModule = NULL;
-
+	static CHAR* g_FONT_NAME[256];
 	switch (uMsg)
 	{
 	case WM_CREATE:
@@ -92,25 +97,26 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		);
 
 		hFontsModule = LoadLibrary("Fonts.dll");
-		HRSRC hFntRes = FindResource(hFontsModule, MAKEINTRESOURCE(2001), MAKEINTRESOURCE(RT_FONT));
-		HGLOBAL hFntMem = LoadResource(hFontsModule, hFntRes); // загружаем Font прям в оперативную память
-		VOID* fntData = LockResource(hFntMem);// далее его нужно заблокировать
-		DWORD nFonts = 0;
-		DWORD len = SizeofResource(hFontsModule, hFntRes);
-		AddFontMemResourceEx(fntData, len, NULL, &nFonts);
+		//HRSRC hFntRes = FindResource(hFontsModule, MAKEINTRESOURCE(2003), MAKEINTRESOURCE(RT_FONT));
+		//HGLOBAL hFntMem = LoadResource(hFontsModule, hFntRes); // загружаем Font прям в оперативную память
+		//VOID* fntData = LockResource(hFntMem);// далее его нужно заблокировать
+		//DWORD nFonts = 0;
+		//DWORD len = SizeofResource(hFontsModule, hFntRes);
+		//AddFontMemResourceEx(fntData, len, NULL, &nFonts);
 
 		//AddFontResource("Fonts\\light-led-display-7.ttf");
+		LoadFontFromDLL(hFontsModule);
 		HFONT hFont = CreateFont
 		(
 			g_i_FONT_HEIGHT, g_i_FONT_WIDTH,
-			0,0,
-			FW_MEDIUM, 0,0,0,
+			0, 0,
+			FW_MEDIUM, 0, 0, 0,
 			ANSI_CHARSET,
 			OUT_CHARACTER_PRECIS,
 			CLIP_CHARACTER_PRECIS,
 			ANTIALIASED_QUALITY,
-			FF_DONTCARE	,
-			"MOSCOW2024"  // нужно прописывать имя шрифта, а не файла
+			FF_DONTCARE,
+			g_FONT_NAME[2]  // нужно прописывать имя шрифта, а не файла
 		);
 		SendMessage(hEdit, WM_SETFONT, (LPARAM)hFontsModule, TRUE);
 
@@ -440,12 +446,22 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		//1) создаем всплывающее меню
 		HMENU hMenu = CreatePopupMenu();
+		HMENU hMenuSkins = CreatePopupMenu();
+		HMENU hMenuFonts = CreatePopupMenu();
 		//2) добавляем пункты в созданное меню
 		InsertMenu(hMenu, 0 /*stack principle*/, MF_BYPOSITION | MF_STRING, IDR_EXIT, "Exit");
 		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_SEPARATOR, 0, NULL);
-		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_METAL_MISTRAL, "Metal mistral");
-		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_SQUARE_BLUE, "Square blue");
-		CheckMenuItem(hMenu, index, MF_BYPOSITION | MF_CHECKED);
+		InsertMenu(hMenuFonts, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_MOSCOW_2024, "Digital-7");
+		InsertMenu(hMenuFonts, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_TERMINATOR, "Terminator Two");
+		InsertMenu(hMenuFonts, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_DIGITAL_7, "Moscow 2024");
+
+		InsertMenu(hMenuSkins, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_METAL_MISTRAL, "Metal mistral");
+		InsertMenu(hMenuSkins, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_SQUARE_BLUE, "Square blue");
+		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_POPUP, (UINT_PTR)hMenuFonts, "Fonts");
+		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_POPUP, (UINT_PTR)hMenuSkins, "Skins");
+
+		CheckMenuItem(hMenuSkins, index, MF_BYPOSITION | MF_CHECKED);
+
 		//3) использование контекстного меню
 		DWORD item = TrackPopupMenu(hMenu, TPM_RETURNCMD /*будет возвращать id ресурса выбранного пункта*/ | TPM_RIGHTALIGN | TPM_BOTTOMALIGN, LOWORD(lParam), HIWORD(lParam), 0, hwnd, NULL);// -IDR_METAL_MISTRAL;
 		switch (item)
@@ -551,4 +567,21 @@ VOID SetSkinFromDLL(HWND hwnd, CONST CHAR skin[])
 	}
 
 	FreeLibrary(hModule);
+}
+VOID LoadFontFromDLL(HMODULE hFontModule, INT resourceID)
+{
+	HRSRC hFntRes = FindResource(hFontModule, MAKEINTRESOURCE(resourceID), MAKEINTRESOURCE(RT_FONT));
+	HGLOBAL hFntMem = LoadResource(hFontModule, hFntRes); // загружаем Font прям в оперативную память
+	VOID* fntData = LockResource(hFntMem);// далее его нужно заблокировать
+	DWORD nFonts = 0;
+	DWORD len = SizeofResource(hFontModule, hFntRes);
+	AddFontMemResourceEx(fntData, len, NULL, &nFonts);
+
+}
+VOID LoadFontFromDLL(HMODULE hFontsModule)
+{
+	for (int i = 2001; i <= 2003; i++)
+	{
+		LoadFontFromDLL(hFontsModule, i); // таким образом загружаем все шрифты из DLL в память
+	}
 }
